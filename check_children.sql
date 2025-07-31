20f1e3edac2b8eb3c71f09ed21d756c58dfa2fe4
limeechian <limeechian0211@gmail.com>
2025-07-31 18:52:38 +0800

    Remove testing and debug files from repository

--- check_children.sql
+++
@@ -1,5 +0,0 @@
--- Check the children table to see the actual child_id values
-SELECT child_id, name, age, gender FROM children_profile ORDER BY child_id;
-
--- Check the conversations table to see current child_id assignments
-SELECT conversation_id, child_id, title FROM ai_conversations ORDER BY conversation_id; 
